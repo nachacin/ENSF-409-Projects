2a0a68140de6af5b608a84f1a580ad432b95b24f
NasihNazeem <nasihnazeem@gmail.com>
2021-07-19 22:49:19 -0600

    Initialized variables and created constructor for Supplier class

--- Lab 3/Exercise 1/src/Supplier.java
+++ Lab 3/Exercise 1/src/Supplier.java
@@ -3,6 +3,14 @@ public class Supplier {
     private String address;
     private String companyname;
     private String salescontact;
-    private Order [] orders;
+
+
+    public Supplier(int aId, String anAddress, aCompanyName, aSalesContact){
+        this.id = aId;
+        this.address = anAddress;
+        this.companyname = aCompanyName;
+        this.salescontact = aSalesContact;
+    }
+
 
 }
