7e420445e40c3124f3504304efa4ed5db961dfbb
NasihNazeem <nasihnazeem@gmail.com>
2021-07-21 15:45:25 -0600

    Creation of Lab 3\Exercise2 folder with Java files included

---
+++ Lab 3/Exercise 2/src/Student.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Student {
+	
+	private String studentName;
+	private int studentId;
+	//private ArrayList<CourseOffering> offeringList;
+	private ArrayList<Registration> studentRegList;
+	
+	public Student (String studentName, int studentId) {
+		this.setStudentName(studentName);
+		this.setStudentId(studentId);
+		studentRegList = new ArrayList<Registration>();
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	@Override
+	public String toString () {
+		String st = "Student Name: " + getStudentName() + "\n" +
+				"Student Id: " + getStudentId() + "\n\n";
+		return st;
+	}
+
+	public void addRegistration(Registration registration) {
+		// TODO Auto-generated method stub
+		studentRegList.add(registration);
+	}
+
+}
