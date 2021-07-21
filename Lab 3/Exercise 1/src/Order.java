01dfdeb0815be5a4b455d26178825120c22ddb4d
NasihNazeem <nasihnazeem@gmail.com>
2021-07-21 02:19:58 -0600

    Addition of placeOrder method, and classes to support the methods in Order.java

--- Lab 3/Exercise 1/src/Order.java
+++ Lab 3/Exercise 1/src/Order.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Order {
-    private String orderID;
-    private String date;
+    private int orderID;
+    private LocalDate date;
+    private String supplier;
+
+    ArrayList<Order> orderlist = new ArrayList<Order>();
+    
+
+    /**
+     * Create Order ID, 5-digit ID
+     * Find LocalDate
+     * 
+     * @param toolArray
+     */
+    public void placeOrder(ArrayList<Tool> toolArray, int index, int ordered) {
+        orderID = createID(); 
+        date = LocalDate.now();
+        
+        
+        
+        
+    }
+
+    public int createID(){
+        Random rand = new Random();
+        return 10000 + rand.nextInt(100000);
+    }
 
 }
