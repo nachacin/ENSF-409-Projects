7b93dcc2fc4a80e6a032bfbb5ffca014eb4ba954
Nestor <nachacin@ucalgary.ca>
2021-07-21 12:58:26 -0600

    Incomplete changes to Order

--- Lab 3/Exercise 1/src/Order.java
+++ Lab 3/Exercise 1/src/Order.java
@@ -1,11 +1,10 @@
+import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Order {
-    private static int orderID;
-    private static LocalDate date;
-    private String supplier;
+    private static int ordersMade = 0;
 
     ArrayList<Order> orderlist = new ArrayList<Order>();
     
@@ -17,17 +16,31 @@ public class Order {
      * @param toolArray
      */
     public static void placeOrder(ArrayList<Tool> toolArray, int index, int ordered) {
+        if (ordersMade == 0) {
+            createOrdersFile();
+        }
         orderID = createID(); 
         date = LocalDate.now();
-        
-        
-        
-        
     }
 
     public static int createID(){
         Random rand = new Random();
         return 10000 + rand.nextInt(100000);
+    }
+    
+    private static void createOrdersFile() {
+        File pastFile 
+        try {
+            File ordersFile = new File("orders.txt");
+            if (ordersFile.createNewFile()) {
+              System.out.println("File created: " + ordersFile.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+          } catch (IOException e) {
+            System.out.println("An error occurred. File orders.txt could not be created");
+            e.printStackTrace();
+          }
     }
 
 }
