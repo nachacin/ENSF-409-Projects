c3d7c4a59399b958af8c5598a548b794354c0fce
NasihNazeem <nasihnazeem@gmail.com>
2021-08-05 11:36:16 -0600

    Final Versions of Task 2 of Exercise 2

--- Lab 7/Exercise 2/Task 2/src/Main.java
+++ Lab 7/Exercise 2/Task 2/src/Main.java
@@ -15,19 +15,19 @@ public class Main {
         Thread t5 = new Thread(r5);
 
         t1.start();
-        t1.join();
-
         t2.start();
-        t2.join();
-
         t3.start();
-        t3.join();
-
         t4.start();
-        t4.join();
-
         t5.start();
+
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
         t5.join();
+
+
 
 
 
--- Lab 7/Exercise 2/Task 2/src/RandomNumber.java
+++ Lab 7/Exercise 2/Task 2/src/RandomNumber.java
@@ -2,14 +2,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomNumber implements Runnable {
-<<<<<<< Updated upstream
-   // int random;
-=======
     private int random;
->>>>>>> Stashed changes
     private static ArrayList<Integer> list = new ArrayList<>();
     private static int sum;
-    private Random r;
+    private Random r = new Random();
 
 
     @Override
@@ -18,15 +14,8 @@ public class RandomNumber implements Runnable {
         
         // TODO Auto-generated method stub
         try{
-<<<<<<< Updated upstream
-            int random;
-            r = new Random();
-            //r.setSeed(98769);
-            random = r.nextInt(100);
-=======
             
             random = r.nextInt(100 - 1) + 1;
->>>>>>> Stashed changes
             addToList(random);
             System.out.println("This iteration value is: " + random + "\n");
             
