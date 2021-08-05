f738ce641c3386065f342709212fb087d64004c0
Nestor <nachacin@ucalgary.ca>
2021-08-04 20:20:08 -0600

    Added a Randomized Sleep Length in SimpleThread
    Answer for task 1:
    
    On my hardware, the output would not get stuck between threads. To cause
    it, I had to lengthen the argument in the sleep() call by a random
    amount between 0 and 50. This created enough variation for the scheduler
    to preempt a thread in the middle of a key computation, in this case the
    increment of +1 to the counter in Resource. For instance, if one thread
    loaded counter into a register, incremented it, and is then
    preempted before it can store the new value in counter, another thread
    could begin with a counter that has not been updated. If this other
    thread continues past the store point, counter will have the same value
    as the one to be stored once the original thread is given a chance to
    run again.

--- Lab 7/Exercise 1/Resource.java
+++ Lab 7/Exercise 1/Resource.java
@@ -1,4 +1,7 @@
-
+/**
+ * Since no constructor is provided, int is initialized to 0 after a new
+ * instance is created.
+ */
 public class Resource {
 
 	int counter;
--- Lab 7/Exercise 1/SimpleThread.java
+++ Lab 7/Exercise 1/SimpleThread.java
@@ -1,14 +1,13 @@
-
 public class SimpleThread extends Thread{
 
 	Resource resource;
 	
 	public void run() {
-		for(int i = 0; i<10; i++){
+		for(int i = 0; i<20; i++){
 			try {
 			System.out.println(resource.increment());
 			
-			Thread.sleep(1);
+			Thread.sleep((int) (50 * Math.random())); // had to add this to create a race condition
 			
 			} catch (InterruptedException e) {
 				e.printStackTrace();
