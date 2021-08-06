c7e2bd9d09f9b47becad815f01e52f8b863e55d0
Nestor <nachacin@ucalgary.ca>
2021-08-06 16:55:41 -0600

    Added documentation!!

---
+++ Project/PrePro/documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"BinSearchTree"},{"p":"<Unnamed>","l":"Data"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"inputReader"},{"p":"<Unnamed>","l":"Node"}]
