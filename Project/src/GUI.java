87c57b9e394ece4a17b04593373323a056fc5507
NasihNazeem <nasihnazeem@gmail.com>
2021-08-05 23:14:11 -0600

    Added actionPerformed method into GUI.java

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,31 +1,35 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 
 
 public class GUI {
 
+    private JButton button1;
+    private JButton button2;
+    JButton button3;
+    JButton button4;
+
+
     public GUI() {
         JFrame frame = new JFrame();
         JTextArea textArea = new JTextArea(20,60);
         JTextField mainText = new JTextField("An Application to Maintain Student Records");
-        JButton button1 = new JButton("Insert");
-        JButton button2 = new JButton("Find");
-        JButton button3 = new JButton("Browse");
-        JButton button4 = new JButton("Create Tree from File");
+        button1 = new JButton("Insert");
+        button2 = new JButton("Find");
+        button3 = new JButton("Browse");
+        button4 = new JButton("Create Tree from File");
         JPanel panel = new JPanel();
         JScrollPane scroll = new JScrollPane(textArea);
         JPanel subPanel = new JPanel();
         
         panel.setLayout(new BorderLayout());
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
+
         mainText.setHorizontalAlignment(JTextField.CENTER);
         mainText.setEditable(false);
-        
-                
-        
-        
-        
+
         button1.setFocusable(false);
         button2.setFocusable(false);
         button3.setFocusable(false);
@@ -36,6 +40,12 @@ public class GUI {
         subPanel.add(button3);
         subPanel.add(button4);
         panel.add(subPanel, BorderLayout.CENTER);
+
+        button1.addActionListener(actions);
+        button2.addActionListener(actions);
+        button3.addActionListener(actions);
+        button4.addActionListener(actions);
+
         
         
 
@@ -50,4 +60,23 @@ public class GUI {
     public static void main(String[] args) {
         new GUI();
     }
+
+
+    private ActionListener actions = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == button1)
+                System.out.println("button 1 was pressed");
+            if(e.getSource() == button2)
+                System.out.println("button 2 was pressed");
+            if(e.getSource() == button3)
+                System.out.println("button 3 was pressed");
+            if(e.getSource() == button4)    
+                System.out.println("button 4 was pressed");
+        }
+
+    };
+    
+
+    
 }
