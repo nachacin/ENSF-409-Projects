91c9661026b786c94c1ed8e8b46c3038272d03d7
NasihNazeem <nasihnazeem@gmail.com>
2021-08-07 23:59:43 -0600

    Updated Task2C

--- Lab 6/Exercise 2/Task 2/src/Square.java
+++ Lab 6/Exercise 2/Task 2/src/Square.java
@@ -7,4 +7,10 @@ public class Square extends Shape {
     public void drawSquare() {
         // Some drawing is done.
     }
+
+    @Override
+    public void drawShape() {
+        // TODO Auto-generated method stub
+        
+    }
 }
